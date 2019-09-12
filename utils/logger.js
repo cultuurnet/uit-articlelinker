8f439c99c9042d80aa1e6003564a8984b1894ee3
brampauwelyn <brampauw@gmail.com>
2019-09-12 14:31:06 +0200

    WID-245 - change log to json format

--- utils/logger.js
+++ utils/logger.js
@@ -18,24 +18,48 @@ const logger = app => {
   // create a file stream 
   const accessLogStream = fs.createWriteStream(path.join(logFileDir, 'access.log'), {flags: 'a'});
 
+  const jsonFormatRequest = (tokens, req, res) => {
+    return JSON.stringify({
+        'id': tokens['id'](req, res),
+        'request-type': 'request',
+        '@timestamp': tokens['date'](req, res, 'iso'),
+        'param-url': tokens['param-url'](req, res),
+        'param-cdbid': tokens['param-cdbid'](req, res),
+        'remoteaddr': tokens['remote-addr'](req, res),
+        'method': tokens['method'](req, res),
+        'url': tokens['res'](req, res, 'content-length'),
+        'referrer': tokens['referrer'](req, res),
+        'user-agent': tokens['user-agent'](req, res),
+    });
+  };
+
+  const jsonFormatResponse = (tokens, req, res) => {
+    return JSON.stringify({
+        'id': tokens['id'](req, res),
+        'request-type': 'response',
+        '@timestamp': tokens['date'](req, res, 'iso'),
+        'response-message': tokens['response-message'](req, res),
+        'remote-address': tokens['remote-addr'](req, res),
+        'status': tokens['status'](req, res)
+      });
+  };
+
   morgan.token('id',  req => req.id);
   morgan.token('param-url',  (req, res) => req.body.url);
   morgan.token('param-cdbid',  (req, res) => req.body.cdbid);
   morgan.token('response-message',  (req, res) => res.message);
 
   // log incoming request
-  app.use(morgan('request :id [:date] :param-url :param-cdbid :remote-addr ":method :url HTTP/:http-version" ":referrer" ":user-agent"', {
+  app.use(morgan(jsonFormatRequest, {
     immediate: true,
     stream: accessLogStream
   }));
 
   // log response
-  app.use(morgan('response :id [:date] :response-message :remote-addr :status :res[content-length]', {
+  app.use(morgan(jsonFormatResponse, {
     immediate: false,
     stream: accessLogStream
   }));
-
-
 
 };
 
