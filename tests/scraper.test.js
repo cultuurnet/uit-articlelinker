82f771102b546ae4b7c830bf46c180cbf29f4364
Emma Fabre <ehtnam6@gmail.com>
2025-02-19 14:47:51 +0100

    Update tests since VinylFactory has bot detection now

--- tests/scraper.test.js
+++ tests/scraper.test.js
@@ -7,10 +7,10 @@ const scraper = require('../utils/scraper');
 describe('Scraper: get meta data from websites', () => {
 
     test('scraper should return meta object with headline, text, url, favicon properties', () => {
-        const url = 'https://thevinylfactory.com/features/the-best-albums-of-2019-so-far/';
-        return scraper(url).then( res => {
+        const url = 'https://amp.theguardian.com/technology/2019/jul/11/google-home-assistant-listen-recordings-users-privacy';
+        return scraper(url).then(res => {
             const meta = res;
-            expect(meta).toMatchObject({ 
+            expect(meta).toMatchObject({
                 headline: expect.any(String),
                 text: expect.any(String),
                 url: expect.any(String),
@@ -20,33 +20,33 @@ describe('Scraper: get meta data from websites', () => {
     });
 
     test('scraper should return meta object with correct info', () => {
-        const url = 'https://thevinylfactory.com/news/radiohead-share-18-hours-ok-computer-sessions-mini-disc-hacked/';
+        const url = 'https://amp.theguardian.com/technology/2019/jul/11/google-home-assistant-listen-recordings-users-privacy';
         const correctMeta = {
-            headline: 'Radiohead share 18 hours of OK Computer studio sessions',
-            text: ".",
-            url: 'https://thevinylfactory.com/news/radiohead-share-18-hours-ok-computer-sessions-mini-disc-hacked/',
-            favicon: "https://thevinylfactory.com/wp-content/uploads/2017/01/cropped-VFselects-logo-2-1-32x32.png"
+            headline: 'Google workers can listen to what people say to its AI home devices',
+            "text": "Company admitted that contractors can access recordings made by Assistant, after some of its recordings were leaked",
+            "url": "https://www.theguardian.com/technology/2019/jul/11/google-home-assistant-listen-recordings-users-privacy",
+            "favicon": "https://static.guim.co.uk/images/favicon-32x32.ico",
         };
-        return scraper(url).then( res => {
+        return scraper(url).then(res => {
             const meta = res;
             expect(meta).toMatchObject(correctMeta);
         });
     });
 
     test('scraper should return the relative path of favicon', () => {
-      const url = 'https://www.gva.be/cnt/DMF20191003_04642727';
-      const favicon = 'https://markup.gva.be/extra/assets/img/favicons/coast-icon-228x228.png';
-      return scraper(url).then( res => {
-        const meta = res;
-        expect(meta.favicon).toContain(favicon);
-      });
+        const url = 'https://www.gva.be/cnt/DMF20191003_04642727';
+        const favicon = 'https://www.gva.be/favicon.svg';
+        return scraper(url).then(res => {
+            const meta = res;
+            expect(meta.favicon).toContain(favicon);
+        });
     });
 
 
     test('scraper should return meta object with canonical url instead of initial url', () => {
         const url = 'https://amp.theguardian.com/technology/2019/jul/11/google-home-assistant-listen-recordings-users-privacy';
         const canonical = 'https://www.theguardian.com/technology/2019/jul/11/google-home-assistant-listen-recordings-users-privacy';
-        return scraper(url).then( res => {
+        return scraper(url).then(res => {
             const meta = res;
             expect(meta.url).toBe(canonical);
         });
@@ -55,7 +55,7 @@ describe('Scraper: get meta data from websites', () => {
     test('scraper should return meta object with stripped url instead of url with params', () => {
         const url = 'https://www.bruzz.be/culture/events-festivals/30-jaar-boterhammen-het-park-met-jari-demeulemeester-muziek-moet-kunnen?fbclid=IwAR0UEuKftZJHXEYOkqKgAy0bXKI7lx83iKNN4k4u1swgnaTn6XAtcEU5gog';
         const strippedUrl = 'https://www.bruzz.be/culture/events-festivals/30-jaar-boterhammen-het-park-met-jari-demeulemeester-muziek-moet-kunnen';
-        return scraper(url).then( res => {
+        return scraper(url).then(res => {
             const meta = res;
             expect(meta.url).toBe(strippedUrl);
         });
@@ -65,8 +65,7 @@ describe('Scraper: get meta data from websites', () => {
         const url = 'https://www.notavalidurl.be';
         return scraper(url)
             .then()
-            .catch( err => expect(err).toBeDefined() );
+            .catch(err => expect(err).toBeDefined());
     });
 
 });
-
