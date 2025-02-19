507e9e4c7db14384679dbb6194614d8b2c999109
Emma Fabre <ehtnam6@gmail.com>
2025-02-19 14:49:39 +0100

    Linting

--- tests/publishers.test.js
+++ tests/publishers.test.js
@@ -1,21 +1,20 @@
 const findPublishers = require('../utils/findPublishers');
 
 describe('findPublishers: check if a publisher is trusted or not', () => {
-
     test('bruzz.be should be a trusted publisher', () => {
-        const articleUrl = "https://www.bruzz.be/mobiliteit/billy-bike-breidt-uit-naar-10-extra-gemeentes-2019-07-12";
+        const articleUrl = 'https://www.bruzz.be/mobiliteit/billy-bike-breidt-uit-naar-10-extra-gemeentes-2019-07-12';
         const publishers = findPublishers(articleUrl);
         expect(publishers.length).toBeTruthy();
     });
 
     test('gva should be a trusted publisher', () => {
-        const articleUrl = "https://www.gva.be/cnt/dmf20191004_04645015/cats-dogs-opent-winkel-in-de-kempen-biowinkel-onder-dierenspeciaalzaken";
+        const articleUrl = 'https://www.gva.be/cnt/dmf20191004_04645015/cats-dogs-opent-winkel-in-de-kempen-biowinkel-onder-dierenspeciaalzaken';
         const publishers = findPublishers(articleUrl);
         expect(publishers.length).toBeTruthy();
     });
 
     test('an article of bruzz.be should return the correct trusted publisher', () => {
-        const articleUrl = "https://bruzz.be/en/uit/eat-drink/cafe-flora-road-memphis-2019-07-12";
+        const articleUrl = 'https://bruzz.be/en/uit/eat-drink/cafe-flora-road-memphis-2019-07-12';
         const publishers = findPublishers(articleUrl);
         const foundPublisherName = publishers[0].name;
         expect(foundPublisherName).toEqual('BRUZZ');
@@ -23,35 +22,27 @@ describe('findPublishers: check if a publisher is trusted or not', () => {
 
     test('an article posted on a blacklisted url of a publisher is NOT considered a trusted publisher', () => {
         const testUrlArr = [
-            "https://admin.bruzz.be/en/uit/eat-drink/cafe-flora-road-memphis-2019-07-12",
-            "https://stag.bruzz.be/economie/brussel-bij-aantrekkelijkste-europese-steden-voor-start-ups-2019-08-21 b323a3c9-e897-4114-a6f9-68ea4ec9acbd",
-            "https://staging.bruzz.be/en/uit/eat-drink/cafe-flora-road-memphis-2019-07-12"
+            'https://admin.bruzz.be/en/uit/eat-drink/cafe-flora-road-memphis-2019-07-12',
+            'https://stag.bruzz.be/economie/brussel-bij-aantrekkelijkste-europese-steden-voor-start-ups-2019-08-21 b323a3c9-e897-4114-a6f9-68ea4ec9acbd',
+            'https://staging.bruzz.be/en/uit/eat-drink/cafe-flora-road-memphis-2019-07-12',
         ];
-        testUrlArr.map( testUrl => {
+        testUrlArr.map((testUrl) => {
             const publishers = findPublishers(testUrl);
             expect(publishers.length).toBeFalsy();
-        });  
+        });
     });
 
     test('an article posted on a homepage of a publisher is NOT a trusted publisher', () => {
-        const testUrlArr = [
-            "https://bruzz.be/",
-            "https://bruzz.be",
-            "https://www.bruzz.be",
-            "http://www.bruzz.be"
-        ];
-        testUrlArr.map( testUrl => {
+        const testUrlArr = ['https://bruzz.be/', 'https://bruzz.be', 'https://www.bruzz.be', 'http://www.bruzz.be'];
+        testUrlArr.map((testUrl) => {
             const publishers = findPublishers(testUrl);
             expect(publishers.length).toBeFalsy();
         });
     });
 
     test('thevinylfactory.be should not be a trusted publisher', () => {
-        const articleUrl = "https://thevinylfactory.com/features/the-best-albums-of-2019-so-far/";
+        const articleUrl = 'https://thevinylfactory.com/features/the-best-albums-of-2019-so-far/';
         const publishers = findPublishers(articleUrl);
         expect(publishers.length).toBeFalsy();
     });
-
-
 });
-
--- tests/scraper.test.js
+++ tests/scraper.test.js
@@ -5,16 +5,15 @@
 const scraper = require('../utils/scraper');
 
 describe('Scraper: get meta data from websites', () => {
-
     test('scraper should return meta object with headline, text, url, favicon properties', () => {
         const url = 'https://amp.theguardian.com/technology/2019/jul/11/google-home-assistant-listen-recordings-users-privacy';
-        return scraper(url).then(res => {
+        return scraper(url).then((res) => {
             const meta = res;
             expect(meta).toMatchObject({
                 headline: expect.any(String),
                 text: expect.any(String),
                 url: expect.any(String),
-                favicon: expect.any(String)
+                favicon: expect.any(String),
             });
         });
     });
@@ -23,11 +22,11 @@ describe('Scraper: get meta data from websites', () => {
         const url = 'https://amp.theguardian.com/technology/2019/jul/11/google-home-assistant-listen-recordings-users-privacy';
         const correctMeta = {
             headline: 'Google workers can listen to what people say to its AI home devices',
-            "text": "Company admitted that contractors can access recordings made by Assistant, after some of its recordings were leaked",
-            "url": "https://www.theguardian.com/technology/2019/jul/11/google-home-assistant-listen-recordings-users-privacy",
-            "favicon": "https://static.guim.co.uk/images/favicon-32x32.ico",
+            text: 'Company admitted that contractors can access recordings made by Assistant, after some of its recordings were leaked',
+            url: 'https://www.theguardian.com/technology/2019/jul/11/google-home-assistant-listen-recordings-users-privacy',
+            favicon: 'https://static.guim.co.uk/images/favicon-32x32.ico',
         };
-        return scraper(url).then(res => {
+        return scraper(url).then((res) => {
             const meta = res;
             expect(meta).toMatchObject(correctMeta);
         });
@@ -36,26 +35,26 @@ describe('Scraper: get meta data from websites', () => {
     test('scraper should return the relative path of favicon', () => {
         const url = 'https://www.gva.be/cnt/DMF20191003_04642727';
         const favicon = 'https://www.gva.be/favicon.svg';
-        return scraper(url).then(res => {
+        return scraper(url).then((res) => {
             const meta = res;
             expect(meta.favicon).toContain(favicon);
         });
     });
 
-
     test('scraper should return meta object with canonical url instead of initial url', () => {
         const url = 'https://amp.theguardian.com/technology/2019/jul/11/google-home-assistant-listen-recordings-users-privacy';
         const canonical = 'https://www.theguardian.com/technology/2019/jul/11/google-home-assistant-listen-recordings-users-privacy';
-        return scraper(url).then(res => {
+        return scraper(url).then((res) => {
             const meta = res;
             expect(meta.url).toBe(canonical);
         });
     });
 
     test('scraper should return meta object with stripped url instead of url with params', () => {
-        const url = 'https://www.bruzz.be/culture/events-festivals/30-jaar-boterhammen-het-park-met-jari-demeulemeester-muziek-moet-kunnen?fbclid=IwAR0UEuKftZJHXEYOkqKgAy0bXKI7lx83iKNN4k4u1swgnaTn6XAtcEU5gog';
+        const url =
+            'https://www.bruzz.be/culture/events-festivals/30-jaar-boterhammen-het-park-met-jari-demeulemeester-muziek-moet-kunnen?fbclid=IwAR0UEuKftZJHXEYOkqKgAy0bXKI7lx83iKNN4k4u1swgnaTn6XAtcEU5gog';
         const strippedUrl = 'https://www.bruzz.be/culture/events-festivals/30-jaar-boterhammen-het-park-met-jari-demeulemeester-muziek-moet-kunnen';
-        return scraper(url).then(res => {
+        return scraper(url).then((res) => {
             const meta = res;
             expect(meta.url).toBe(strippedUrl);
         });
@@ -65,7 +64,6 @@ describe('Scraper: get meta data from websites', () => {
         const url = 'https://www.notavalidurl.be';
         return scraper(url)
             .then()
-            .catch(err => expect(err).toBeDefined());
+            .catch((err) => expect(err).toBeDefined());
     });
-
 });
